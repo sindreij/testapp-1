65b83c2c7db560c9a2c6bfb0e0145c7b9754770e
Sindre Ilebekk Johansen <sindre@sindrejohansen.no>
2020-11-27 13:01:30 +0100

    Example app for logging

--- src/index.js
+++ src/index.js
@@ -2,41 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 
-const db = require('./db');
-
 // Init app
 
 const app = express();
 
 app.use(express.json());
 
-db.initDb().catch(err => {
-    console.error(err);
-    process.exit(1);
-});
-
 // Routes
 
-app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname + '/../public/' }));
-
-app.get('/api/messages', (req, res, next) => {
-    db.getMessages()
-        .then(messages => res.json(messages))
-        .catch(next);
+app.get('/', (req, res) => {
+    res.send('Hello world');
 });
 
 app.get('/api/cronjob', (req, res, next) => {
-    console.log('Cronjob called');
+    console.log('${new Date()}: Cronjob called. ');
     console.log(req.headers);
     res.send('okay');
-});
-
-app.post('/api/messages', (req, res, next) => {
-    db.postMessage(req.body.message)
-        .then(() => {
-            res.send('OK');
-        })
-        .catch(next);
 });
 
 const port = process.env.PORT || 3030;
